c289c7a3dedd9aca37a318a269a37af2d7ba3d27
Dipesh <dipsha@trobetho.com>
2021-03-22 16:28:35 +0530

    new db and support for ipv6

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
-curl -O http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz
-file GeoLite2-Country.mmdb.gz
-gunzip GeoLite2-Country.mmdb.gz
+# Old format
+# curl -O http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz
+# file GeoLite2-Country.mmdb.gz
+# gunzip GeoLite2-Country.mmdb.gz
+
+tar -xf GeoLite2-Country_20210316.tar.gz GeoLite2-Country_20210316/GeoLite2-Country.mmdb
+mv GeoLite2-Country_20210316/GeoLite2-Country.mmdb .
+file GeoLite2-Country.mmdb
