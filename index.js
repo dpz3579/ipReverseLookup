6a62f493c37fd2086dffccf971c72efc36e05d8f
Dipesh <dipsha@trobetho.com>
2021-06-29 13:34:22 +0530

    bug fix: removed unwanted blank reloadsync

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const getReverseIp = (remoteIP, category ,cb) => {
   }else if(category && typeof category == "string" && category.toLowerCase() == "city"){
     dbPath = dirPath + cityDBName;
     reader.reloadSync(dbPath);
-    reader.reloadSync();
   }
 
   const resp = reader.lookup(remoteIP); // resp will be null if no information is found for the IP or if the IP is malformed.
